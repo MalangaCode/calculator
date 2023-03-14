2bc338542fb67ba2339391ea58bde2c2d3a494c2
luis <102092058+MalangaCode@users.noreply.github.com>
2023-03-14 16:41:40 -0500

    Add basic math operators functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add() {
+    return a + b
+}
+ function subtraction() {
+    return a - b
+}
+
+function multiply() {
+    return a * b
+}
+
+function division() {
+    return a / b
+}
+
+function percentage() {
+    return n / 100
+}
+
