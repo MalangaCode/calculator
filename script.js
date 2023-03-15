b9a624cede263a821a63cbc46e1f0ee815137d89
luis <102092058+MalangaCode@users.noreply.github.com>
2023-03-15 11:10:16 -0500

    Add funtionality to buttons
    the number buttons display the number on the screen

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add() {
-    return a + b
+    return a[0] + a[1]
 }
  function subtraction() {
     return a - b
@@ -17,3 +17,35 @@ function percentage() {
     return n / 100
 }
 
+const numBtn = document.querySelectorAll('button');
+const a = [];
+let operator;
+numBtn.forEach((number) => {
+    
+
+    number.addEventListener('click', () => {
+
+        const screen = document.getElementById('screenText');
+        
+        if(number.getAttribute('class') === 'number'){
+            screen.textContent += number.id
+        } else if (number.getAttribute('class') === 'clear') {
+            screen.textContent = '';
+            a.splice(0, a.length);
+
+        } else if (number.getAttribute('class') === 'operator') {
+            a.push(Number(screen.textContent));
+            (number.getAttribute('id') === 'plus') ? operator = 'plus' : 
+            (number.getAttribute('id') === 'minus') ? operator = 'minus' :
+            (number.getAttribute('id') === 'multiply') ? operator = 'multiply' :
+            (number.getAttribute('id') === 'divide') ? operator = 'divide' : 
+            'error';
+            console.log(a)
+            console.log(operator);
+            screen.textContent = '';
+        } else if (number.getAttribute('class') === 'equal') {
+            
+        }
+    })
+});
+console.log(numBtn);
