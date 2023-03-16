32a7e9c849cdb6a23f394b00e49e464081ee31ac
luis <102092058+MalangaCode@users.noreply.github.com>
2023-03-16 15:14:55 -0500

    Add setOperator Function

--- script.js
+++ script.js
@@ -1,3 +1,92 @@
+
+const buttons = document.querySelectorAll('button');
+const a = [];
+let operator;
+let result;
+
+buttons.forEach((btn) => {
+    
+    btn.addEventListener('click', () => {
+
+        const btnId = btn.getAttribute('id');
+        const btnClass = btn.getAttribute('class');
+
+        const screen = document.getElementById('screenText');
+        
+        if(btnClass === 'number'){
+            
+            screen.textContent += btn.id
+
+        } else if (btnClass === 'clear') {
+            
+            screen.textContent = '';
+            a.splice(0, a.length);
+
+        } else if (btnClass === 'operator') {
+
+            if (screen.textContent === '') {
+                screen.textContent = '';
+
+                setOperator (btnId)
+
+            } else {
+                a.push(Number(screen.textContent));
+
+                setMathOperation (operator);
+                setOperator (btnId);
+                screen.textContent = '';
+            }
+
+        } else if (btnClass === 'equal') {
+            
+            a.push(Number(screen.textContent));
+
+            setMathOperation (operator);
+
+            screen.textContent = result.toPrecision();
+            operator = undefined;
+            a.shift();
+
+        } else if (btnClass === 'dot') {
+            if (screen.textContent.includes('.')){
+
+            } else {
+                screen.textContent += '.'
+            }
+        } else if (btnClass === 'delete') {
+            
+            screen.textContent = ''
+
+        } else if (btnClass === 'percentage') {
+        
+            percentage((Number(screen.textContent)));
+            screen.textContent = result;
+
+        } else if (btnClass === 'symbol') {
+            screen.textContent = Number(- + screen.textContent)
+        }
+    })
+});
+
+function setMathOperation (operator) {
+
+    (operator === undefined)  ? ''            : 
+    (operator === 'plus')     ? add()         :
+    (operator === 'minus')    ? subtraction() :
+    (operator === 'multiply') ? multiply()    :
+    (operator === 'division') ? division()    :
+    '';
+}
+
+function setOperator (btnId) {
+
+    (btnId === 'plus') ? operator = 'plus' : 
+    (btnId === 'minus') ? operator = 'minus' :
+    (btnId === 'multiply') ? operator = 'multiply' :
+    (btnId === 'divide') ? operator = 'divide' : 
+    '';
+}
+
 function add() {
     result = a[0] + a[1];
     a[1] = result;
@@ -25,96 +114,4 @@ function percentage(n) {
     result = n / 100;
     a.push(result);
     a.shift();
-}
-
-const numBtn = document.querySelectorAll('button');
-const a = [];
-let operator;
-let result;
-numBtn.forEach((number) => {
-    
-
-    number.addEventListener('click', () => {
-
-        const screen = document.getElementById('screenText');
-        
-        if(number.getAttribute('class') === 'number'){
-            
-            screen.textContent += number.id
-
-        } else if (number.getAttribute('class') === 'clear') {
-            
-            screen.textContent = '';
-            a.splice(0, a.length);
-
-        } else if (number.getAttribute('class') === 'operator') {
-
-            if (screen.textContent === '') {
-                screen.textContent = '';
-
-                (number.getAttribute('id') === 'plus') ? operator = 'plus' : 
-                (number.getAttribute('id') === 'minus') ? operator = 'minus' :
-                (number.getAttribute('id') === 'multiply') ? operator = 'multiply' :
-                (number.getAttribute('id') === 'divide') ? operator = 'divide' : 
-                'error';
-
-            } else {
-                a.push(Number(screen.textContent));
-
-                setMathOperation (operator);
-
-
-                (number.getAttribute('id') === 'plus') ? operator = 'plus' : 
-                (number.getAttribute('id') === 'minus') ? operator = 'minus' :
-                (number.getAttribute('id') === 'multiply') ? operator = 'multiply' :
-                (number.getAttribute('id') === 'division') ? operator = 'division' : 
-                'error';
-
-                console.log(a)
-                console.log(operator);
-                screen.textContent = '';
-                console.log(result);
-            }
-
-            
-        } else if (number.getAttribute('class') === 'equal') {
-            
-            a.push(Number(screen.textContent));
-
-            setMathOperation (operator);
-
-            screen.textContent = result.toPrecision();
-            operator = undefined;
-            a.shift();
-
-        } else if (number.getAttribute('class') === 'dot') {
-            if (screen.textContent.includes('.')){
-
-            } else {
-                screen.textContent += '.'
-            }
-        } else if (number.getAttribute('class') === 'delete') {
-            
-            screen.textContent = ''
-
-        } else if (number.getAttribute('class') === 'percentage') {
-        
-            percentage((Number(screen.textContent)));
-            screen.textContent = result;
-
-        } else if (number.getAttribute('class') === 'symbol') {
-            screen.textContent = Number(- + screen.textContent)
-        }
-    })
-});
-
-function setMathOperation (operator) {
-
-    (operator === undefined)  ? ''            : 
-    (operator === 'plus')     ? add()         :
-    (operator === 'minus')    ? subtraction() :
-    (operator === 'multiply') ? multiply()    :
-    (operator === 'division') ? division()    :
-    '';
-
 }
